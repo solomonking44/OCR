94ecb263ec9b3304fe69e8776da7ab40f0faf86b
solomonking44 <mugishasolomon520@gmail.com>
2023-04-13 19:49:49 +0300

    "Photos can be uploaded to DB and text document can be downloaded"

--- venv/include/app.py
+++ venv/include/app.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import cv2 as cv
 from io import BytesIO
 import pytesseract
-#create the app
 from PIL import Image
+import random
+
+#flask app declaration
 app = Flask(__name__)
 
 #configure the SQLite database, relative to the app instance folder
@@ -14,7 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #create the extension
 db = SQLAlchemy(app)
 
-
+#Database model
 class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
@@ -28,37 +30,81 @@ class Upload(db.Model):
         return f"Filename is {self.filename}"
     
 
-# def download(upload_id)
-
+#route traffic from '/' to 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
+    # random_number = random.randint(0,99999999)
+    
+    #if it is a POST method
     if request.method == 'POST':
+        
+        #store image name in variable
         file = request.files['image']
         
-        # text = ''
+        #Everything with access to flask app
         with app.app_context():
+            
+            #
+            ## PART ONE ## - STORE IMAGE IN DB 
+            #
+            
+            #create tables
             db.create_all()
+            
+            #create an object
             upload = Upload(filename=file.filename, data=file.read())
+            
+            #create the database session
             db.session.add(upload)
+            
+            #save the information
             db.session.commit()
             
-            #process the image
-            download = Upload.query.filter_by(filename=file.filename).first()
+            #
+            ## PART TWO ## - PROCESS IMAGE ##
+            #
+            
+            #fetch info from table filter by filename
+            download = Upload.query.filter_by(filename=random_number).first()
+            
+            #store image in uploads
             with open(f"./uploads/{download.filename}.png", "wb") as f:
                 f.write(download.data)
-                # image = cv.imread(f.write(download.data),0)
-                # text = pytesseract.image_to_string(f.write(download.data))
+
+            #read image in opencv
             image = cv.imread(f'./uploads/{download.filename}.png',0)
-            # if image:
                 
+            #read the text from the image
             text = pytesseract.image_to_string(image)
-                
-                # grey = cv.COLOR_BGR2GREY(image)
             
-        return send_file(open(f"{download.filename}.txt", 'w'))
-    
+            #store filename in variable
+            file_name = download.filename
+            
+            #extract the file name from the file type
+            cut_name = file_name.split(".", 1)[0]
+            
+            #delete that record from db
+            delete = Upload.query.filter_by(filename=file.filename).delete()
+            
+            #open .txt file 
+            image_text = open(f'./text_files/{cut_name}.txt', 'w')
+            
+            #write text in file
+            image_text.write(text)
+            
+            #testing#
+            records = Upload.query.all()
+            # print(records)
+            for record in records:
+                print(record)
+            print(text)
+            print(download.filename)
+      
+            #return the file to download
+            return send_file(f"./text_files/{cut_name}.txt", as_attachment=True)
+        
+    #return index page
     return render_template('index.html')
+
     
-if __name__ == '__main__':
-    db.create_all()
-    app.run()
